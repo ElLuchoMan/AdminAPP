ba074a70f350feb34abf5d9ea3dc0fa1c7341775
ElLuchoMan <luchoman96@gmail.com>
2021-03-08 23:03:54 -0500

    Agregada url

--- sap/sap/urls.py
+++ sap/sap/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from webapp.views import bienvenido
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', bienvenido)
 ]
--- sap/webapp/views.py
+++ sap/webapp/views.py
@@ -1,3 +1,6 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def bienvenido(request):
+    return HttpResponse('Respuesta desde django')
