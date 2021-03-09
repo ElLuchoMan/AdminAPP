cd96be5203c561973bbcd56e8070dc7561e4102b
ElLuchoMan <luchoman96@gmail.com>
2021-03-09 12:39:26 -0500

    Agregado modelo de persona

--- sap/personas/models.py
+++ sap/personas/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Persona(models.Model):
+    nombre = models.CharField(max_length=250)
+    apellido = models.CharField(max_length=250)
+    email = models.CharField(max_length=250)
+    password = models.CharField(max_length=50)
