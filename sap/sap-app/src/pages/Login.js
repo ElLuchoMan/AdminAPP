7e7351f721cbbd83e068c81fe67c171d2cbd542c
ElLuchoMan <luchoman96@gmail.com>
2021-03-10 15:40:23 -0500

    Login finalizado TODO: Conectar a djnago

--- sap/sap-app/src/pages/Login.js
+++ sap/sap-app/src/pages/Login.js
@@ -1,23 +1,45 @@
 import React, { Component } from 'react'
-import '../css/Login.css'
+import '../css/Login.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
 
-class Login extends Component {
+export default class Login extends Component {
+    state = {
+        form: {
+            email: ' ',
+            password: ' ',
+        }
+    }
+    handleChange = async e => {
+        await this.setState({
+            form: {
+                ...this.state.form,
+                [e.target.name]: e.target.value
+            }
+        });
+        console.log(this.state.form)
+    }
     render() {
         return (
-            <div className="ContainerPrincipal">
-                <div className="ContainerSecundario">
-                    <label>Usuario</label>
-                    <br />
-                    <input type="text" className="form-control" />
-                    <br />
-                    <label>Contraseña</label>
-                    <br/>
-                    <input type="password" className="form-control" />
-                    <br />
-                    <button className="btn btn-primary">Iniciar Sesión</button>
+            <div className="Pantalla">
+                <h1>Inicio de sesión</h1>
+                <hr />
+
+
+                <div className="ContainerPrincipal">
+                    <div className="ContainerSecundario">
+                        <label>Email</label>
+                        <br />
+                        <input type="text" name="email" className="form-control" placeholder="Ingrese Email" onChange={this.handleChange} />
+                        <br />
+                        <label>Contraseña</label>
+                        <br />
+                        <input type="password" name="password" className="form-control" placeholder="Ingrese Contraseña" onChange={this.handleChange} />
+                        <br />
+                        <button className="btn btn-primary">Iniciar Sesión</button>
+                    </div>
                 </div>
             </div>
         )
     }
 }
-export default Login;
