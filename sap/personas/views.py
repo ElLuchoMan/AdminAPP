4bb5c4b1779b07d48c3269c991776ce721626ac1
ElLuchoMan <luchoman96@gmail.com>
2021-03-10 22:01:57 -0500

    Listo para conectar con react

--- sap/personas/views.py
+++ sap/personas/views.py
@@ -3,7 +3,6 @@ from rest_framework import generics
 from .models import Persona
 from .serializers import PersonaSerializer
 
-
 class PersonaList(generics.ListCreateAPIView):
     queryset = Persona.objects.all()
     serializer_class = PersonaSerializer
