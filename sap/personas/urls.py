419d22494aa68b441bf4ed48ec34a2c5beb9ba63
ElLuchoMan <luchoman96@gmail.com>
2021-03-12 16:24:26 -0500

    Finalizado

--- sap/personas/urls.py
+++ sap/personas/urls.py
@@ -3,5 +3,4 @@ from .views import PersonaListView, PersonaDetailView
 
 urlpatterns=[
   path('persona/',PersonaListView.as_view(),name='persona_list'),
-  path('persona/<str:name>/', PersonaDetailView.as_view(),name='persona'),
 ]
